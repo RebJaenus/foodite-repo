aa813c070821e2011c4fc9daca633f9340c28d8c
RebJaenus <bberglund@comcast.net>
2019-04-21 23:38:25 +0000

    added pagination with formatting and started making simple form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,8 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.page(params[:page]).per(1)
   end
-
+  def new
+    @place = Place.new
+  end
 end
