1a94c8bf01fc286b8cf5d36c7e4663483eae33ac
RebJaenus <bberglund@comcast.net>
2019-04-29 20:54:59 +0000

    added create functions to places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,11 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
