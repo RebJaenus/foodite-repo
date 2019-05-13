040b93e9207da76c529efbe7e5d776d02cf06ec7
RebJaenus <bberglund@comcast.net>
2019-05-13 21:02:46 +0000

    added gmail account for sending emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@fooditeapp.com"
 
   def comment_added
-    mail(to: "bberglund@comcast.net",
+    mail(to: "fooditeapp@gmail.com",
       subject: "A comment has been added to your place on foodite")
   end
 end
