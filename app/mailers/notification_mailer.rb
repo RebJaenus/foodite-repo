6eeaf19a5d3076d1fdbd81be7a5b60da6213fc92
RebJaenus <bberglund@comcast.net>
2019-05-13 21:35:01 +0000

    changed comment email to add link and send to correct user email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@fooditeapp.com"
 
-  def comment_added
-    mail(to: "fooditeapp@gmail.com",
-      subject: "A comment has been added to your place on foodite")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}")
   end
 end
